74034dba10e2aa5a3bb8eeb771e81b8d513825de
Edmilson Torres <edmilson.torres@gmail.com>
2022-11-01 00:13:30 -0300

    feat: express server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express from 'express';
-
+import express, { NextFunction, Request, Response } from 'express';
 import routes from './routers';
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (!err) {
+        return next();
+    }
+    res.status(500);
+    res.send({ error: 'Internal server error' });
+});
 
 export default app;
--- src/routers.ts
+++ src/routers.ts
@@ -1,7 +1,28 @@
 import { Router } from 'express';
-
+import topicExchangePublisher from './workers/producers/order-producer';
+import topicExchangePublisherSamples from './workers/producers/order-producer-samples';
 const router = Router();
 
-router.get('/ping', (req, res) => res.json({ message: 'pong' }));
+router.get('/publish/samples', async (req, res) => {
+    const result = await topicExchangePublisherSamples();
+    if (result instanceof Error) {
+        return res.status(500).json({ error: result.message });
+    }
+    res.json({ message: 'messages samples sent' });
+});
+
+router.post('/publish', async (req, res) => {
+    const dataType = req.body.data.type;
+    if (dataType === 'online' || dataType === 'offline') {
+        const result = await topicExchangePublisher(req.body);
+        if (result instanceof Error) {
+            return res.status(500).json({ error: result.message });
+        }
+        return res.json({ message: 'message sent' });
+    }
+    return res
+        .status(400)
+        .json({ error: 'data type must be online or offline' });
+});
 
 export default router;
